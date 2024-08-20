dd5a971b9c2f60873b7e59a7c6fc302700ddabe0
Rodrigo <digotg_6@hotmail.com>
2024-08-20 10:07:27 -0300

    test

---
+++ XadrezProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Principal;
+using XadrezProject;
+using XadrezProject.tabuleiro;
+using XadrezProject.xadrez;
+
+public class Program {
+    public static void Main() {
+        try {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 3));
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 7));
+
+            Tela.imprimirTabuleiro(tab);
+        }
+        catch (TabuleiroException e) {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
---
+++ XadrezProject/tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XadrezProject.tabuleiro {
+     enum Cor {
+        Branca,
+        Preta, 
+        Amarela,
+        Verde,
+        Laranja
+    }
+}
