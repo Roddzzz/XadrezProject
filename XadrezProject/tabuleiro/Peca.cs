8aea4025fb3f22143ebe66adcc3fcfea6f96d318
Rodrigo <digotg_6@hotmail.com>
2024-08-20 11:56:28 -0300

    Correção metodo podeMoverPara

--- XadrezProject/tabuleiro/Peca.cs
+++ XadrezProject/tabuleiro/Peca.cs
@@ -41,8 +41,9 @@ namespace XadrezProject.tabuleiro {
             return false;
         }
 
-        public bool podeMoverPara(Posicao pos) {
+        public bool movimentoPossivel(Posicao pos) {
             return movimentosPossiveis()[pos.linha, pos.coluna];
+
         }
 
         public abstract bool[,] movimentosPossiveis();
